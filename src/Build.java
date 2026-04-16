bc908ec77eed50d121e71eec716491ec5d7b9a1f
Akai <161771111+Akaiokaa@users.noreply.github.com>
2026-04-16 00:00:18 -0700

    Completed printSelfLoopers

--- src/Build.java
+++ src/Build.java
@@ -63,6 +63,21 @@ public class Build {
    * @param <T> the type of values stored in the vertices
    */
   public static <T> void printSelfLoopers(Vertex<T> vertex) {
+    if(vertex == null) return;
+    printSelfLoopers(vertex, new HashSet<>());
+  }
+
+  public static <T> void printSelfLoopers(Vertex<T> vertex, Set<Vertex<T>> visited) {
+    if(visited.contains(vertex)) return;
+    visited.add(vertex);
+
+    if (vertex.neighbors.contains(vertex)) {
+      System.out.println(vertex.data);
+    }
+    
+    for(Vertex<T> neighbor: vertex.neighbors){
+      printSelfLoopers(neighbor, visited);
+    }
   }
 
   /**
